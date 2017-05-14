62dafaa891f09e18d10a69148ecb8f44e4c5de3b
drworm75 <dwgames@gmail.com>
2017-05-14 17:12:19 -0500

    Filters working

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,44 @@
-alert('JS Ready');
+app.run((FIREBASE_CONFIG) => {
+	firebase.initializeApp(FIREBASE_CONFIG);
+});
+
+app.controller("ShroomTypes", ($http, $q, $scope, FIREBASE_CONFIG) => {
+
+	$scope.filters = {};
+
+	$scope.toadstools = [];
+
+	let getShroomsList = () => {
+		let fungi = [];
+		return $q((resolve, reject) => {
+			$http.get(`${FIREBASE_CONFIG.databaseURL}/mushrooms.json`)
+			.then((fbItems) => {
+			var itemCollection = fbItems.data;
+	        Object.keys(itemCollection).forEach((key) => {
+				itemCollection[key].id=key;
+				fungi.push(itemCollection[key]); 
+			});
+			resolve(fungi);
+			console.log(fungi);
+		})
+			.catch((error) => {
+				reject(error);
+			});
+		});
+	};
+
+	let getShrooms = () => {
+		getShroomsList().then((fungi) => {
+			$scope.toadstools = fungi;
+		}).catch((error) => {
+			console.log("get error", error);
+		});
+	};
+
+
+	getShrooms();
+
+
+
+
+});
